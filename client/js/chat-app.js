175fcbf1f6710d3ea3df57d2d61efad1ec8371cb
StephyStuff <stephanie.birch13@gmail.com>
2016-11-02 17:10:14 -0400

    Sass watch did not originally cretae a styles.css.map file. Recreated sass folder and started sass watch over. Styles.css.map was successfully created and moved into client folder.

--- client/js/chat-app.js
+++ client/js/chat-app.js
@@ -1,54 +1,54 @@
 (function() {
   'use strict';
-    window.chat = window.chat || {};
+  window.chat = window.chat || {};
 
-    console.log('wyd');
+  window.chat.listenForMessages(function messageHandler(data) {
+    $('.messages')
+    .append('<p>' + data.message + '</p>')
+  }
+
+);
+console.log('wyd');
 
     var userToken;
     $('.login')
-      .on('submit', function submitLogin(event){
+    .on('submit', function submitLogin(event){
         event.preventDefault();
         $.ajax({
-            url:'/login',
-            method: 'POST',
-            data: JSON.stringify({username: $('.username').val() }),
-            headers: {
-              'Content-Type': 'application/json'
+          url:'/login',
+          method: 'POST',
+          data: JSON.stringify({username: $('.username').val() }),
+          headers: {
+            'Content-Type': 'application/json'
           }
-        })
-        .done(function handleMessageSuccess(data){
-          userToken = data.token;
-          console.log('Let\'s Chat!', userToken);
-        })
-        .fail(function handleMessageFailure(xhr){
-          console.log(xhr);
-        })
-    });
+    })
+    .done(function handleMessageSuccess(data){
+      userToken = data.token;
+      console.log('Let\'s Chat!', userToken);
+    })
+    .fail(function handleMessageFailure(xhr){
+      console.log(xhr);
+    })
+  });
 
     $('.send-message')
-      .on('submit', function sendMessage(event){
-        event.preventDefault();
-        $.ajax({
-            url: '/chat',
-            method: 'POST',
-            data: JSON.stringify({ message: $('.message').val() }),
-            headers: {
-              Authorization: userToken,
-              'Content-Type': 'application/json'
-          }
-        })
-        .done(function successHandler(data){
-          console.log('hey', data);
-        })
-        .fail(function failHandler(data){
-          console.log('try again!');
-        })
-      });
-
-
-
-
-
-
-
+    .on('submit', function sendMessage(event){
+      event.preventDefault();
+      $.ajax({
+        url: '/chat',
+        method: 'POST',
+        data: JSON.stringify({ message: $('.message').val() }),
+        headers: {
+          Authorization: userToken,
+          'Content-Type': 'application/json'
+        }
+    })
+    .done(function successHandler(data){
+      console.log('hey', data);
+    })
+    .fail(function failHandler(data){
+      console.log('try again!');
+    })
+  });
+  
 }());
